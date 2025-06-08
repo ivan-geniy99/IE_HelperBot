6a9dafc7d3044ff9474bf2de825feea2f1637a0d
ivan-geniy99 <ivandugar163@gmail.com>
2025-06-08 18:27:57 +0400

    add regex

--- stop_words.py
+++ stop_words.py
@@ -1,3 +1,21 @@
 import re
 
-MESSAGE_REMOVE_PATTERN = re.compile(r"(?i)\b(badword|spamword|regex_example)\b")
+MESSAGE_REMOVE_PATTERN = re.compile(
+    r"(?i)"
+    r"pump\.fun|"
+    r"\w{40}pump|"
+    r"to\sjoin\sprivate\ssignal|"
+    r"first\s10\speople\sto\sdm\si\swill|"
+    r"who\sneed\s(sol|the\ssol)|"
+    r"next\srunner\s\w{40}|"
+    r"hello\sadmin\sand\scommunity|"
+    r"some\sof\smy\sservices\sinclude|"
+    r"promotion\son\smy\stelegram\syou\sneed\sgood\spromotion|"
+    r"i\shave\strusted\sreal\sinvestor|"
+    r"if\syou\sneed\spromotion\,\sdm\sme|"
+    r"mass\sdm|"
+    r"dm\snow|"
+    r"crypto\spromo\sloaded|"
+    r"launching\ssoon|"
+    r"shill\sme\syour\sx100\sgem"
+)
